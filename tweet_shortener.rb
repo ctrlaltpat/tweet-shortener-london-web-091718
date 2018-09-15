416da2499e29b44a76fd38eede1f6673ab0dfaea
patrickp246 <patrickp246@gmail.com>
2018-09-15 16:42:17 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  rtn_tweet = tweet.split(" ")
+  rtn_tweet.map! {
+    |word|
+    if dictionary.keys.include?(word.downcase)
+      dictionary[word.downcase]
+    else
+      word
+    end
+  }
+  rtn_tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.count > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweets)
+  tweets
+end
