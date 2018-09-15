2b1e4fed3abc76723cc3845824ab7a93b2b79a0d
patrickp246 <patrickp246@gmail.com>
2018-09-15 16:44:02 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.count > 140
+  if tweet.split("").count > 140
     word_substituter(tweet)
   else
     tweet
